30f36502be0c0dc9ae2adea2827e912293324084
HARSHVARDHANSINHA <harshvsinha18@.com>
2023-06-02 17:17:46 +0530

    about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ComponentsComponent } from './components/components.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
+    NavbarComponent,
+    ComponentsComponent,
   ],
   imports: [
     BrowserModule,
